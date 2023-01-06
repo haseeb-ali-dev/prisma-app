6a827d6fc97f0aa957791bcbf512ec38b7df5a65
haseeb-ali-dev <h45.busy@gmail.com>
2023-01-06 03:30:31 -0800

    user modal and migration

---
+++ index.ts
@@ -0,0 +1,11 @@
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+
+const main = async () => {
+  const users = await prisma.user.findMany()
+  console.log(users)
+}
+
+main()
+  .catch(e => console.log(e))
+  .finally(async () => await prisma.$disconnect())
